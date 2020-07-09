aeab74573045acccf2dcec08978c2996529fa339
ParshaviBolya <43488428+ParshaviBolya@users.noreply.github.com>
2020-07-09 11:18:57 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+// import CountUp from 'react-countup';
+// import {Helmet} from "react-helmet";
+import './counter.css'
+import $ from "jquery";
+
+
+// const Demo = props => (
+// <div className="application">
+//             <Helmet>
+//               <script src="./count.js" type="text/javascript" />
+//             </Helmet>
+            
+//         </div>
+  
+// );
+
+
+class Counter extends Component{
+    componentDidMount(){
+        $('.counter-count').each(function () {
+            $(this).prop('Counter',0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 5000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+    }
+    render()
+    {
+        
+        return(
+            <div>
+                 {/* {Demo} */}
+            <div className="counter">
+    <div className="container">
+        <div className="row">
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <div className="employees">
+                    <p className="counter-count">12</p>
+                    <p className="employee-p">Institutes</p>
+                </div>
+            </div>
+
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <div className="customer">
+                    <p className="counter-count">354</p>
+                    <p className="customer-p">Subjects</p>
+                </div>
+            </div>
+
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <div className="design">
+                    <p className="counter-count">675</p>
+                    <p className="design-p">Topics</p>
+                </div>
+            </div>
+
+            <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <div className="order">
+                    <p className="counter-count">4351</p>
+                    <p className="order-p">Courses</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+       
+        </div>
+        );
+    }
+}
+
+
+export default Counter;
