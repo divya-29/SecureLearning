aa619c97abb3b61910d7cff550a4f46fe3ce7765
Virendra Singh <38318324+virendra56422@users.noreply.github.com>
2020-07-14 10:28:51 +0530

    Update ServiceCard.jsx

--- src/Components/ServiceCard.jsx
+++ src/Components/ServiceCard.jsx
@@ -5,23 +5,21 @@ import img2 from './assets/images/img2.jpg';
 import img3 from './assets/images/img3.jpg';
 
 const ServiceCard = props =>{
-	// render() {
-		return(
-			<div className="container-fluid d-flex justify-content-center container-fluid-div">
-				<div className="row row-div-1">
-					<div className="col-md-4">
-						<ServiceCardUI imgsrc={img1} title='Learn'/>
-					</div>
-					<div className="col-md-4">
-						<ServiceCardUI imgsrc={img2} title='India'/>
-					</div>
-					<div className="col-md-4">
-						<ServiceCardUI imgsrc={img3} title='Learn'/>
-					</div>
+	return(
+		<div className="container-fluid d-flex justify-content-center container-fluid-div">
+			<div className="row row-div-1">
+				<div className="col-md-4 col-md-4-c">
+					<ServiceCardUI imgsrc={img1} title='Learn'/>
+				</div>
+				<div className="col-md-4 col-md-4-c">
+					<ServiceCardUI imgsrc={img2} title='India'/>
+				</div>
+				<div className="col-md-4 col-md-4-c">
+					<ServiceCardUI imgsrc={img3} title='Learn'/>
 				</div>
 			</div>
-		)
-	// }
+		</div>
+	)
 }
 
 export default ServiceCard;
