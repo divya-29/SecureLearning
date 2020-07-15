5f9e61389015d685f3a98a5f4407580b2347f108
ParshaviBolya <43488428+ParshaviBolya@users.noreply.github.com>
2020-07-15 16:06:08 +0530

    Update mentors.js

--- src/Components/mentors.js
+++ src/Components/mentors.js
@@ -4,42 +4,55 @@ import '../CSS/mentors.css';
 import Reviews from './reviews';
 
 class Mentors extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          items: []
+        };
+      }
+      componentDidMount() {
+        fetch("https://lil-project-1.herokuapp.com/api/teammembers")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                // isLoaded: true,
+                items: result.data.teamMembers
+              });
+            }
+           
+           
+          )
+      }    
     render()
     {
+        const item = this.state.items.map((i) =>{
         return(
-            <div>
-                <br></br>
-            <div className="container-fluid">
-           <div className="row">
+            <div className="row">
             
-            <div className=" example col-md-12 col-12">
-            <h1 id="head" >Our Mentors Belief
-            </h1>  
-              </div>      
-        </div> 
-           </div>
            <br></br>
            <br></br>
 
-           <div className="container-fluid">
-               <div className="row " style={{backgroundColor:"#F4F3F3"}}>
+           {/* <div className="container-fluid">
+               <div className="row " style={{backgroundColor:"#F4F3F3"}}> */}
+               
                    <div className="col-sm-3 col-lg-3 col-md-3 margins">
                        <img
-                       src={ment}
+                       src={i.imageUrl}
                        className="responsive-class rounded">
                        </img>
 
                    </div>
 
                
-               <div className="col-sm-9 col-lg-9 col-md-9 mentor margins" style={{backgroundColor:"#F4F3F3"}}> 
+               <div className="col-sm-9 col-lg-9 col-md-9 mentor margins second mr-auto" style={{backgroundColor:"#F4F3F3"}}> 
                         <h2>
-                            Mentor 1
+                           {i.name}
                         </h2>
                        <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
-                        tempor incididunt ut labore et dolore magna aliqua.
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                       {i.description}
                         </p>
 
                      </div> 
@@ -51,96 +64,38 @@ class Mentors extends Component{
                      
                     
 
-                     <div className="row " style={{backgroundColor:"#F0F8FF"}}>
-                   
-               
-               <div className="col-sm-9 col-lg-9 col-md-9 mentor margins" style={{backgroundColor:"#F0F8FF"}}> 
-                       <h2>
-                           Mentor 2
-                       </h2>
-                       <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
-                        tempor incididunt ut labore et dolore magna aliqua.
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        </p>
-
-                     </div> 
-                     <div className="col-sm-3 col-lg-3 col-md-3 margins">
-                       <img
-                       src={ment}
-                       className="responsive-class rounded second">
-                       </img>
-
-                   </div>
-                   <br></br>
-                   <br></br>
-                   <br></br>
-                     <br></br>
-                     </div>
                     
-                   
-
-                     <div className="row " style={{backgroundColor:"#F4F3F3"}}>
-                   <div className="col-sm-3 col-lg-3 col-md-3 margins">
-                       <img
-                       src={ment}
-                       className="responsive-class rounded">
-                       </img>
-
-                   </div>
-
-               
-               <div className="col-sm-9 col-lg-9 col-md-9 mentor margins" style={{backgroundColor:"#F4F3F3"}}> 
-                       <h2>
-                           Mentor 3
-                           </h2> 
-                       <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
-                        tempor incididunt ut labore et dolore magna aliqua.
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        </p>
-
-                     </div> 
-                     <br></br>
-                     <br></br>
-                     <br></br>
-                     <br></br>
-                     </div>
-             
-              
-
-              <div className="row " style={{backgroundColor:"#F0F8FF"}}>
-                   
-               
-                   <div className="col-sm-9 col-lg-9 col-md-9 mentor margins" style={{backgroundColor:"#F0F8FF"}}>
-                           <h2>
-                               Mentor 4
-                               </h2>  
-                           <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
-                            tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            </p>
-    
-                         </div> 
-                         <div className="col-sm-3 col-lg-3 col-md-3 margins">
-                           <img
-                           src={ment}
-                           className="responsive-class rounded second">
-                           </img>
-    
-                       </div>
-                       <br></br>
-                       <br></br>
-                       <br></br>
-                     <br></br>
-                       </div>
-           </div>
-           <Reviews />
-           </div>
+        //    </div>
+        //    </div>
 
         );
-    }
+        
+    })
+
+    return(
+        <div>
+        <div className="container-fluid">
+           <div className="row">
+            
+            <div className=" example col-md-12 col-12">
+            <h1 id="head" >Our Mentors Belief
+            </h1>  
+              </div>      
+        </div> 
+           </div>
+            <div className="container-fluid">
+            <div className="row " style={{backgroundColor:"#F4F3F3"}}>
+
+                {item}
+                </div>
+
+                </div>
+                <Reviews />
+                </div>
+        
+    )
 }
+}
+
 
 export default Mentors;
