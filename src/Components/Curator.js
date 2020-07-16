4a5cc91c532f62fe5f4d5bc8839287dd4ea640ef
Divya <jaind2710@gmail.com>
2020-07-16 13:08:21 +0530

    Final Draft

--- src/Components/Curator.js
+++ src/Components/Curator.js
@@ -11,7 +11,7 @@ class Curator extends React.Component {
   render() {
 	return(
 		<div>
-		<div className="container-fluid d-flex justify-content-center main-div">
+		<div className="container-fluid d-flex justify-content-center mx-auto main-div">
 			<div className="row row-div">
 				<div className="col-md-4">
 					<CuratorUI imgsrc={img1} name={'Yash Singh'} role={'DevOps Manager'} content={'Curious learner, passionate teacher, not a professional, but a work in progress rather. Working up the ladder, in a nonconforming fashion. Being a thinker and a technology enthusiast, i strive to discover and thus engage myself in practical applications of technology for the betterment of my career and society by leveraging the same applications remodeled in functional usecases for the problem statements i come across.'} />
