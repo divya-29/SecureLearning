4c1bdd5cb6f3c11a5c18b05e50a0ee540208c8e3
Divya Jain <jaind2710@gmail.com>
2020-07-17 13:39:30 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -53,7 +53,7 @@ class Navb extends Component{
                             <div className="d-flex justify-content-end ml-auto">
                             <Nav className="ml-auto " className="topnav" id="nav" navbar >
                                 <NavItem style={{margin:"15px", textDecoration:"none", color:'white'}} id="ab"><NavLink to='/HomePage'> Homepage</NavLink> </NavItem>
-                                <NavItem style={{margin:"15px", color:'white'}} id="ab"><NavLink to='/Study'> Our Services</NavLink> </NavItem>
+                                <NavItem style={{margin:"15px", color:'white'}} id="ab"><NavLink to='/Study'> Content</NavLink> </NavItem>
                                 <NavItem style={{margin:"15px", color:'white'}} id="ab"> <NavLink to='/Bel'> Our Beliefs and Reviews</NavLink> </NavItem>
                                 <NavItem style={{margin:"15px", color:'white'}} id="ab"><NavLink to='/Gallery'>Gallery</NavLink> </NavItem>
                                 <NavItem style={{margin:"15px", color:'white'}} id="ab"> <a href='#about'> About Us</a> </NavItem> 
